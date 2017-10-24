1c40725ec2c11b588d1fb45669c2fd9fcb0d8913
Davi Cavalcanti <davipc@gmail.com>
2017-10-24 17:46:44 +1100

    Changed Direction enum to use the order of directions when finding the turnRight/turnLeft direction

--- src/main/java/com/catalyser/mars/domain/Direction.java
+++ src/main/java/com/catalyser/mars/domain/Direction.java
@@ -1,7 +1,9 @@
 package com.catalyser.mars.domain;
 
 public enum Direction {
-	N("North"), S("South"), E("East"), W("West");
+	N("North"), E("East"), S("South"), W("West");
+	
+	private static Direction[] vals = values();
 	
 	private String description;
 	
@@ -12,34 +14,15 @@ public enum Direction {
 	public String getDescription() {
 		return description;
 	}
-	
-	public Direction rotateRight() {
-		switch(this) {
-			case N: return E;
-			case E: return S;
-			case S: return W;
-			case W: return N;
-			// should never happen
-			default: return N;
-		}
-	}
 
-	public Direction rotateLeft() {
-		switch(this) {
-			case N: return W;
-			case W: return S;
-			case S: return E;
-			case E: return N;
-			// should never happen
-			default: return N;
-		}
-	}
+    public Direction rotateRight()
+    {
+        return vals[(this.ordinal()+1) % vals.length];
+    }
 	
-	// could use this kind of construct as well... but would be exposed to bugs in case new constants were added to the enum
-//	private static Direction[] vals = values();
-//    public Direction rotateRight()
-//    {
-//        return vals[(this.ordinal()+1) % vals.length];
-//    }
+    public Direction rotateLeft()
+    {
+        return vals[(this.ordinal()+ vals.length -1) % vals.length];
+    }
 	
 }
